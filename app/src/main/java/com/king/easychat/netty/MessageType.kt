9f31b447db19569025659e056b36ebf069f328b9
jenly1314 <jenly1314@mail.com>
2019-10-08 15:32:52 +0800

    first commit

---
+++ app/src/main/java/com/king/easychat/netty/MessageType.kt
@@ -0,0 +1,21 @@
+package com.king.easychat.netty
+
+/**
+ * @author <a href="mailto:jenly1314@gmail.com">Jenly</a>
+ */
+object MessageType {
+
+    const val LOGIN_REQ =  1
+    const val LOGIN_RSP = 2
+
+    const val LOGOUT_REQ =  3
+    const val LOGOUT_RSP = 4
+
+    const val SEND_MESSAGE_REQ =  5
+    const val SEND_MESSAGE_RSP = 6
+
+    const val ADD_FRIEND_REQ =  7
+    const val ADD_FRIEND_RSP = 8
+
+
+}
