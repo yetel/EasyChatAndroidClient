a50825d21e813d9fe8350cadd705c562e5f2fbec
shuuufu <shu_fu1201@163.com>
2019-10-11 18:33:57 +0800

    加了个user 跟 group 对象

--- app/src/main/java/com/king/easychat/netty/packet/resp/GroupMessageResp.kt
+++ app/src/main/java/com/king/easychat/netty/packet/resp/GroupMessageResp.kt
@@ -8,7 +8,19 @@ import com.king.easychat.netty.packet.Packet
  * date: 2019/08/19.
  * description:
  */
-class GroupMessageResp(val groupName : String, val groupId: String, val reason: String, val success: Boolean) : Packet() {
+class GroupMessageResp: Packet() {
+    /** 消息发送方 */
+    val senderId: String? = null
+    /** 消息发送人姓名 */
+    val senderName: String? = null
+    /** 消息接收方 */
+    val groupId: String? = null
+    /** 消息内容 */
+    val message: String? = null
+    /**
+     * 消息类型
+     */
+    private val messageType = 0
     override fun messageType(): Int {
         return MessageType.GROUP_MESSAGE_RESP
     }
